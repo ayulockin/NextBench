ca291e6d23001a7250b49421e46792d5d805400f
ayulockin <mein2work@gmail.com>
2024-12-18 17:23:17 +0530

    openai client

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # NextBench
 NextBench is a collection of wide variety of benchmarks for accessing the performance of LLMs and VLMs and more.
+
+## Features
+
+- [ ] Support for multiple clients/sdks/providers
+- [ ] Support for multiple benchmarks off the shelf
+- [ ] Support for custom benchmarks
+- [ ] Support for multiple metrics
+- [ ] Flexible configuration to run benchmarks
+- [ ] Support for running benchmarks in parallel
+- [ ] Support for customizing the evaluation logic via prompting
--- src/nextbench/clients/openai_client.py
+++ src/nextbench/clients/openai_client.py
@@ -1,6 +1,31 @@
+import os
 import weave
-
 import openai
+from typing import Any
+from pydantic import PrivateAttr
+from dotenv import load_dotenv
+load_dotenv()
+
+from nextbench.utils import RequestResult
+
 
 class OpenAIClient(weave.Model):
-    pass
+    _client: Any = PrivateAttr()
+
+    def model_post_init(self, __context: Any) -> None:
+        self._client = openai.AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    @weave.op()
+    async def predict(self, prompt: str) -> RequestResult:
+        response = await self._client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[{"role": "user", "content": prompt}],
+        )
+        return RequestResult(success=True, cached=False, completions=[response.choices[0].message.content])
+
+if __name__ == "__main__":
+    import asyncio
+
+    client = OpenAIClient()
+    result = asyncio.run(client.predict("What is the capital of France?"))
+    print(result)
